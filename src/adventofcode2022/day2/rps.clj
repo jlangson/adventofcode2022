76192166fbf03ff99672d66f6b17e28c389f4df9
Jared Langson <langson.jared@gmail.com>
2022-12-17 19:40:30 -0500

    Implement calc-winner

--- src/adventofcode2022/day2/rps.clj
+++ src/adventofcode2022/day2/rps.clj
@@ -1,2 +1,30 @@
 (ns adventofcode2022.day2.rps)
 ;rock paper scissors
+
+(def elf-code {"A" "Rock"
+               "B" "Paper"
+               "C" "Scissors"})
+
+(def my-code {"X" "Rock"
+              "Y" "Paper"
+              "Z" "Scissors"})
+
+(defn calc-winner
+  "Determines who won RPS. > 0 is me wins, < 0 elf wins. 0 is draw."
+  [elf me]
+  (cond
+    (= elf me) 0
+    (and (= elf "Rock") (= me "Paper")) 1
+    (and (= elf "Rock") (= me "Scissors")) -1
+    (and (= elf "Paper") (= me "Rock")) -1
+    (and (= elf "Paper") (= me "Scissors")) 1
+    (and (= elf "Scissors") (= me "Rock")) 1
+    (and (= elf "Scissors") (= me "Paper")) -1
+    :else (throw (Exception. "???? What happened in winner?!"))))
+
+
+
+(comment
+
+
+  )
